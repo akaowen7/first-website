5d8fb22ad84a0ebe8e4ef0d85f5dd9e8d3646f34
Reid Tull <reid.a.tull@gmail.com>
2021-09-19 20:56:36 -0400

    Better animations, new button

--- components/Button.js
+++ components/Button.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import styles from '../styles/Button.module.css'
 import { motion } from '../node_modules/framer-motion';
 
 const Button = (props) => {
+
     const { background, text, link } = props
+
+    const [isActive, setIsActive] = useState(false);
+    const [hover, click, timeout] = [25, 18, 100]
+
     return <motion.a 
-    href={link}
-    target="_blank"
     style={{background: background}} 
     className={styles.button} 
-    whileHover={{ 
-        x: 25,    
+    whileHover={{ x: isActive ? click : hover }}
+    animate={{x: isActive ? click : 0}}
+    onClick={() => {
+        setIsActive(true)
+        setTimeout(window.open, timeout, link, null)
+        setTimeout(setIsActive, timeout, false)
     }}>
         {text}
     </motion.a>
