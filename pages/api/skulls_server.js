52c6f2cb9f1e47e3f949debdbfaa31786991dfe4
Reid Tull <reid.a.tull@gmail.com>
2022-04-04 22:44:22 -0400

    small changes

---
+++ pages/api/skulls_server.js
@@ -0,0 +1,11 @@
+// const io = require('socket.io')(3000, {
+//   cors: {
+//     origin : ["https://localhost:8080"]
+//   }
+// });
+
+// io.on('connection', client => {
+//   console.log(client.id)
+//   client.on('event', data => { /* … */ });
+//   client.on('disconnect', () => { /* … */ });
+// });
--- pages/skulls.js
+++ pages/skulls.js
@@ -1,31 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Button from '../components/Button'
-import styles from '../styles/Home.module.css'
-// import { motion } from 'framer-motion';
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import Button from '../components/Button'
+// import styles from '../styles/Home.module.css'
+// import io from 'socket.io-client'
+// // import { motion } from 'framer-motion';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Reid</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// const socket = io('https://localhost:3000')
 
-      <main className={styles.main}>
-        <div className={styles.title}>
-          <h1>Skulls!</h1>
-          <p>I make YouTube videos, music, good twitter takes, and websites now I guess. I've donated to Wikipedia. Twice.</p>
-        </div>
-        <div className={styles.buttons}>
-          <Button background="#FF0000" text="Youtube" link="https://www.youtube.com/channel/UC8ZvHJKfa1xhdMwB5k-7BLQ"/>
-          <Button background="#1DA1F2" text="Twitter" link="https://twitter.com/not_akaowen"/>
-          <Button background="#FE5000" text="SoundCloud" link="https://soundcloud.com/user-237901012"/>
-          <Button background="linear-gradient(90deg, rgba(252,176,69,1) 0%, rgba(253,29,29,1) 50%, rgba(131,58,180,1) 100%)" text="Instagram" link="https://www.instagram.com/blue_plain/"/>
-          <Button background="#ff0050" text="TikTok" link="https://vm.tiktok.com/ZMJ3161xe/"/>
-          <Button background="#1f6feb" text="Github" link="https://github.com/akaowen7"/>
-        </div>
-      </main>
-    </div>
-  )
-}
+// export default function Home() {
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Reid</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main className={styles.main}>
+//         <div className={styles.title}>
+//           <h1>Skulls!</h1>
+//           <p>I make YouTube videos, music, good twitter takes, and websites now I guess. I've donated to Wikipedia. Twice.</p>
+//         </div>
+//         <div className={styles.buttons}>
+//           <Button background="#FF0000" text="Youtube" link="https://www.youtube.com/channel/UC8ZvHJKfa1xhdMwB5k-7BLQ"/>
+//           <Button background="#1DA1F2" text="Twitter" link="https://twitter.com/not_akaowen"/>
+//           <Button background="#FE5000" text="SoundCloud" link="https://soundcloud.com/user-237901012"/>
+//           <Button background="linear-gradient(90deg, rgba(252,176,69,1) 0%, rgba(253,29,29,1) 50%, rgba(131,58,180,1) 100%)" text="Instagram" link="https://www.instagram.com/blue_plain/"/>
+//           <Button background="#ff0050" text="TikTok" link="https://vm.tiktok.com/ZMJ3161xe/"/>
+//           <Button background="#1f6feb" text="Github" link="https://github.com/akaowen7"/>
+//         </div>
+//       </main>
+//     </div>
+//   )
+// }
